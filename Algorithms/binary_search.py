7732094312a211f8aed206231b409f062afb72c3
ICY <415259789@qq.com>
2014-10-28 09:10:47 +0800

    Binary Search Added
    implementaion of Binary Search algorithm

---
+++ Algorithms/binary_search.py
@@ -0,0 +1,48 @@
+#!usr/bin/python
+# -*- coding:UTF-8 -*-
+
+'''
+Introduction: Binary search
+
+Created on: Oct 24, 2014
+
+@author: ICY
+'''
+
+#-------------------------FUNCTION---------------------------#
+
+class Solution:
+    # @param num, a list of integer
+    # @return an integer
+    # the array must be sorted from small to large order
+    def binary_search(self, num, value):
+        left = 0
+        right = len(num) - 1
+
+        while left <= right:
+            middle = left + ((right-left)>>1) #Important,calculate middle index without overflow.
+            if num[middle] > value:
+                right = middle - 1   #update right side
+            elif num[middle] < value:
+                left = middle + 1    #update left side
+            else:
+                return middle
+        return -1
+
+#----------------------------SELF TEST----------------------------#
+
+def main():
+    #test_array = [0]
+    test_array = [1,2]
+    #test_array = [1,2,3,4]
+    #test_array = [2,4,5,6,7,8,9]
+    value = 4
+    solution = Solution()
+    print solution.binary_search(test_array,value)
+    pass
+            
+if __name__ == '__main__': 
+    main()
+    
+    
+    
